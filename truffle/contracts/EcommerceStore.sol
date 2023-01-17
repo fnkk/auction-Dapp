8be21f95fc15135f4198d7cf9abeef4f375ac537
fnkk <8231684+qq998001@user.noreply.gitee.com>
2023-01-17 09:59:23 +0800

    完成合约bid部分，准备测试

--- truffle/contracts/EcommerceStore.sol
+++ truffle/contracts/EcommerceStore.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-
+// geth --datadir data --networkid 666 --http --http.port 8545 --allow-insecure-unlock console 2>output.log console
 contract EcommerceStore {
     enum ProductStatus {
         Open,
@@ -43,7 +43,7 @@ contract EcommerceStore {
         mapping (address => mapping (bytes32 => Bid)) bids;
     }
 
-    Product[] public prodects;
+    Product[] private prodects;
 
     constructor() {
         productIndex = 0;
@@ -62,22 +62,32 @@ contract EcommerceStore {
         require(_autionStartTime<_autionEndTime);
         productIndex += 1;
         // Product memory product = Product(productIndex,_name,_category,_imageLink,_descLink,_autionStartTime,_autionEndTime,_startPrice,address(0),0,0,0,ProductStatus.Open,_condition);
-        // Product storage product = prodects[productIndex];
-        Product storage product = stores[msg.sender][productIndex];
+        Product storage product = prodects[productIndex];
+        // Product storage product = stores[msg.sender][productIndex];
         product.id = productIndex;
         product.name = _name;
+        product.category = _category;
+        product.imageLink = _imageLink;
+        product.descLink = _descLink;
+        product.autionEndTime = _autionEndTime;
+        product.startPrice = _startPrice;
+        product.condition = _condition;
+        product.status = ProductStatus.Open;
         productIdInStore[productIndex] = msg.sender;
     }
 
-    function getProduct(uint _productId) view public returns(Product memory){
-        Product memory product = stores[productIdInStore[_productId]][_productId];
+    function getProduct(uint _productId) view internal returns(Product storage){
+        Product storage product = stores[productIdInStore[_productId]][_productId];
         return (product);
     }
 
-    function bid(uint _productId,bytes32 _bids) public payable returns(bool) {
+    function bid(uint _productId,bytes32 _bid) public payable returns(bool) {
         Product storage product = stores[productIdInStore[_productId]][_productId];
         require(block.timestamp>=product.autionStartTime && block.timestamp<=product.autionEndTime,"wei dao pai mai shi jian");
-        require(msg.value>product.startPrice);
-
+        require(msg.value>product.startPrice,"Value should be larger than start pirce");
+        require(product.bids[msg.sender][_bid].bidder == address(0),"Bidder should be null");
+        product.bids[msg.sender][_bid] = Bid(msg.sender,_productId,msg.value,false);
+        product.totalBids += 1;
+        return true;
     }   
 }
