55bec5c08cc9b10a8044bb9508f9f4a570a9e3e1
fnkk <8231684+qq998001@user.noreply.gitee.com>
2023-03-06 14:44:25 +0800

    delete useless information

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,4 @@
 import { EthProvider } from "./contexts/EthContext";
-import Intro from "./components/Intro/";
-import Added from "./components/Added/Added";
-import Setup from "./components/Setup/Setup";
-import Demo from "./components/Demo";
-import Footer from "./components/Footer";
 import "antd/dist/reset.css";
 import "./App.css";
 import router from './router';
